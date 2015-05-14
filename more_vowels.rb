675d37f09c2cb0655bd147e9611fbfe89d38c1fd
srz2 <srz222@gmail.com>
2015-05-14 00:56:00 -0400

    Done

--- more_vowels.rb
+++ more_vowels.rb
@@ -25,23 +25,27 @@ def vowels_with_case(input)
 end
 
 def vowels_with_no_if_or_case(input)
-  #input.eq(/[aeiou]/)
+  input.match(/[aeiou]/) == nil ? false : true
 end
 
 def vowels_with_array_no_equality_or_if(input)
-
+  a = ["a", "e", "i", "o", "u"]
+  a.include?(input)
 end
 
 def vowels_with_ternary_operator(input)
-
+  vowels_with_case(input) ? true : false
 end
 
 def first_vowel(input)
-
+  index = input =~ /[aeiou]/
+  if index != nil
+    input[index]
+  end
 end
 
 def first_vowel_index(input)
-
+  input =~ /[aeiou]/
 end
 
 
